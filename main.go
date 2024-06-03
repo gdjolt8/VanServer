4d3b22942c5afe04d87fd05564327c8b65b94696
gdjolt8 <102922030+gdjolt8@users.noreply.github.com>
2024-06-03 16:05:45 -0400

    Update main.go

--- main.go
+++ main.go
@@ -73,7 +73,7 @@ func main() {
 
 			for _, document := range documents {
 				println(document)
-				if document["name"] == s["name"] && int(document["points"]) >= level_goals[int(document["points"])] {
+				if document["name"] == s["name"] && int(document["points"].(float64)) >= level_goals[int(document["points"].(float64))] {
 					update := bson.M{"$inc": bson.M{"level": 1}}
 					_, err = collection.UpdateOne(context.TODO(), filter, update)
 					if err != nil {
